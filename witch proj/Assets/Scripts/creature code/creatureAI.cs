9086036a353a144ce771436d278bf6a70c8f6854
shekorla laptop <kassidy.mcconkie@gmail.com>
2024-09-12 11:18:15 -0600

    forgot to backup yesterday
    working on the creature room

--- witch proj/Assets/Scripts/creature code/creatureAI.cs
+++ witch proj/Assets/Scripts/creature code/creatureAI.cs
@@ -10,24 +10,19 @@ public class creatureAI: MonoBehaviour
     public faceData faces;
     [Tooltip("set this to the visuals")]
     public GameObject SmileBody;
-    [Header("cleanup these later")]
-    public Transform[] waypoints;//want to replace this with a function to choose a random point in play area
-    public string currentState;//keep public till fin troubleshooting
     
-    private GameObject mainCam;
+    private string currentState;
     private Animator animator;
     private NavMeshAgent agent;
     private Material faceMaterial;
-    private WaitForSecondsRealtime delay;
+    private int delay;
     
     void Start()
     {
-        delay = new WaitForSecondsRealtime(1);
         currentState = MyData.randAct();
         animator = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
         faceMaterial = SmileBody.GetComponent<Renderer>().materials[1];
-        mainCam = GameObject.Find("Main Camera");
         aiLoop();
     }
 
@@ -36,15 +31,12 @@ public class creatureAI: MonoBehaviour
         faceMaterial.SetTexture(MainTex, tex);
     }
 
-    private int loopCount=0;
     private static readonly int Speed = Animator.StringToHash("speed");
     private static readonly int MainTex = Shader.PropertyToID("_MainTex");
 
     public void aiLoop()
     {
-        StopAllCoroutines();
-        loopCount += 1;
-        Debug.Log("loop: "+loopCount);
+        StopCoroutine(waiting());
         currentState = MyData.randAct();
         switch (currentState)
         {
@@ -52,41 +44,68 @@ public class creatureAI: MonoBehaviour
                 StopAgent();
                 SetFace(faces.Idleface);
                 animator.SetFloat(Speed, 0f);
-                if (Random.Range(0,2)==1)//sometimes look at the camera while idle
-                {
-                    LookAtCamera();
-                }
+                delay = 2;//debugging
+                //delay = Random.Range(10, 30);
                 break;
 
             case "walk":
                 animator.SetFloat(Speed, 0.5f);
                 agent.isStopped = false;
                 agent.updateRotation = true;
-                agent.SetDestination(waypoints[Random.Range(0,waypoints.Length-1)].position);
+                agent.SetDestination(RandomNavmeshLocation(20));
                 SetFace(faces.WalkFace);
-                if (agent.remainingDistance>agent.stoppingDistance)
-                {
-                    StopAllCoroutines();//dont do anything till its done
-                }
+                //dont need to change delay because the wait until works
                 break;
+                
 
             case "jump":
                 StopAgent();
                 SetFace(faces.jumpFace); 
                 animator.SetFloat(Speed, 1f);
+                delay = 2;//debugging
+                //delay = Random.Range(5, 15);
                 break;
        
         }
         StartCoroutine(waiting());
     }
 
+    private bool hasStopped()
+    {
+        if (agent.remainingDistance<agent.stoppingDistance)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
     IEnumerator waiting()
     {
-        yield return delay;
+        if (agent.pathPending)
+        {
+            yield return new WaitUntil(hasStopped);//wait for it to be done moving
+        }
+        else
+        {
+            yield return new WaitForSecondsRealtime(delay);
+        }
         aiLoop();
     }
 
 
+    public Vector3 RandomNavmeshLocation(float radius) {
+        Vector3 randomDirection = Random.insideUnitSphere * radius;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        Vector3 finalPosition = Vector3.zero;
+        if (NavMesh.SamplePosition(randomDirection, out hit, radius, 1)) {
+            finalPosition = hit.position;            
+        }
+        return finalPosition;
+    }//   https://discussions.unity.com/t/how-to-get-a-random-point-on-navmesh/73440/2 found this online here
+    
     private void StopAgent()
     {
         agent.isStopped = true;
@@ -100,9 +119,5 @@ public class creatureAI: MonoBehaviour
         position.y = agent.nextPosition.y;
         transform.position = position;
         agent.nextPosition = transform.position;
-    }
-    void LookAtCamera()
-    {
-        transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, mainCam.transform.rotation.y, transform.rotation.z));   
     }
 }
